d79c07e649196be790a9d1a7098fea16e1903474
qiao.chen <qiao.chen@eeoa.com>
2021-09-17 15:01:38 +0800

    initial commit

---
+++ DebugToolPlugin/src/main/java/cn/eeo/debugtool/plugin/DebugToolPlugin.java
@@ -0,0 +1,11 @@
+package cn.eeo.debugtool.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class DebugToolPlugin implements Plugin<Project> {
+  @Override
+  public void apply(Project project) {
+
+  }
+}
